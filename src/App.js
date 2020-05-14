1ca658f4865a1ef14989da59f6237af5a13c9d19
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-05-14 00:45:13 -0700

    Completed routes to About, and Contact pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./App.css";
 import PortfolioNavbar from "./components/NavBar";
 import Projects from "./pages/Projects"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -11,16 +13,16 @@ function App() {
         <PortfolioNavbar />
         <Switch>
           <Route exact path="/">
-            hello
+          <About />
           </Route>
           <Route exact path="/about">
-            about
+            <About />
           </Route>
           <Route exact path="/projects">
             <Projects/>
           </Route>
           <Route exact path="/contact">
-            contact
+            <Contact/>
           </Route>
         </Switch>
       </div>
