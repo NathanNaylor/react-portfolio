df3a33e323ec3808ddfc2d5d2dc8ecf29bd7fc9f
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-05-14 01:26:51 -0700

    added footer to render at bottom with link to my github and a headshot

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <a href="https://github.com/NathanNaylor">
+        <img
+          className="img-fluid"
+          alt="Headshot"
+          src="./Assets/Images/Bald Right Glasses.png"
+          width="50px"
+        />
+        <span>@NathanNaylor</span>
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
