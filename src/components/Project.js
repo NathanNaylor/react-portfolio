cbb3a23680fb6ec52e17068ae6f1d0ffff930891
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-09-11 23:44:18 -0700

    Added Modal and change buttons to go to links

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState }  from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 const Project = ({ project }) => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
+
   return (
     <div className="mb-3 col-sm-12 col-md-6 ">
       <div className="wrap ">
         <div className="Wrap">
           <a href={project.live} target="_blank" rel="noopener noreferrer"> 
+          <div>
             <img
-              className="img-fluid size"
-              alt="project screenshot"
-              src={project.imageSrc}
-              width="250px"
-            />
+                className="img-fluid size"
+                alt="project screenshot"
+                src={project.imageSrc}
+                width="250px"
+                onClick={toggle}
+              />
+            <Modal isOpen={modal} toggle={toggle} className={"className"}>
+              <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+              <ModalBody>
+                Live link: {project.live}
+              </ModalBody>
+              <ModalFooter>
+                <Button color="white" onClick={toggle}><a href={project.live} target="_blank" rel="noopener noreferrer">Live Link</a></Button>{' '}
+                <Button color="white" onClick={toggle}><a href={project.repo} target="_blank" rel="noopener noreferrer">{project.name} Repo</a></Button>
+              </ModalFooter>
+            </Modal>
+    </div>
+            
           </a>
           <a href={project.repo} target="_blank" rel="noopener noreferrer"><h2>{project.name} Repo</h2></a>
         </div>
