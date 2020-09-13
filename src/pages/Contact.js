fd84b675160c92501e20a4f1f147321d9eaab559
Squelchert <61394430+NathanNaylor@users.noreply.github.com>
2020-09-13 13:24:05 -0700

    Minor styling changes and icon addition

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,41 +2,48 @@ import React from "react";
 
 const Contact = () => {
   return (
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h3>Here are a few ways to contact me!</h3>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-4"></div>
-          <div className="col">
-            <ul>
-              <li>
-                <p>
-                  <a target="_blank" rel="noopener noreferrer" href="https://github.com/NathanNaylor">
-                    GitHub
-                  </a>
-                </p>
-              </li>
-              <li>
-                <p>
-                  <a
-                    target="_blank" rel="noopener noreferrer"
-                    href="https://www.linkedin.com/in/nathan-naylor-463aa61a2/"
-                  >
-                    LinkedIn
-                  </a>
-                </p>
-              </li>
-              <li>
-                <p>Email: nathannaylors@gmail.com</p>
-              </li>
-            </ul>
-          </div>
-          <div className="col-4"></div>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h3>Get in touch</h3>
         </div>
       </div>
+      <div className="row">
+        <div className="col-4">
+          <a href="https://github.com/NathanNaylor" className="footer">
+            <img
+              className="img-fluid"
+              alt="Headshot"
+              src="./Assets/Images/Bald Right Glasses.png"
+              width="50px"
+            />
+            <span>@NathanNaylor</span>
+          </a>
+        </div>
+        <div className="col-4">
+          <p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://github.com/NathanNaylor"
+            >
+              GitHub
+            </a>
+          </p>
+          <p>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.linkedin.com/in/nathan-naylor-463aa61a2/"
+            >
+              LinkedIn
+            </a>
+          </p>
+          <p>nathannaylors@gmail.com</p>
+        </div>
+        <div className="col-4"></div>
+      </div>
+    </div>
   );
 };
 
